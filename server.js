48fe9302eb7f1b6ebb6895d1bce7e22374fcc350
jaysolanki <59509816+jazzy19@users.noreply.github.com>
2020-08-19 09:29:58 +0530

    lec 34

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-const express = require("express");
- 
+const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
 
+//Connect Database
+connectDB();
 
-app.get("/", function(req,res){
-    res.send("Api Running Correctly");
-})
+//Init MiddleWare
+app.use(express.json({ extended: false }));
+
+app.get('/', function (req, res) {
+  res.send('Api Running Correctly');
+});
+
+//Defining Routes
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT , function(){
-    console.log(`Server Started at Port ${PORT}`);
-})
+app.listen(PORT, function () {
+  console.log(`Server Started at Port ${PORT}`);
+});
